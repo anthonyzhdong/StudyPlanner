2717c76a0e4728a1671e4910d041c18cf6241bd9
Sam Merton <mersa636@student.otago.ac.nz>
2024-08-12 12:06:54 +1200

    Reformatted code

--- menu.cpp
+++ menu.cpp
@@ -5,34 +5,38 @@ using namespace std;
 // g++ -o menu menu.cpp
 // ./menu
 
-
-class classInfo{
+class classInfo
+{
 public:
     string paperName;
     string type;
     string location;
     int day;
     int time;
-    classInfo(string paperName, string type, string location, int day, int time){
+    classInfo(string paperName, string type, string location, int day, int time)
+    {
         this->paperName = paperName;
         this->type = type;
         this->location = location;
         this->day = day;
         this->time = time;
     }
-    void displayInfo(){
+    void displayInfo()
+    {
         std::cout << type << " for " << paperName << " at " << location << " on day " << day << " at " << time << std::endl;
     }
 };
 
-class assignment{
+class assignment
+{
 public:
     string assignmentName;
     string overview;
     string paperName;
     int dueDate;
     int grade;
-    assignment(string assignmentName, string overview, string paperName, int dueDate, int grade){
+    assignment(string assignmentName, string overview, string paperName, int dueDate, int grade)
+    {
         this->assignmentName = assignmentName;
         this->overview = overview;
         this->paperName = paperName;
@@ -41,7 +45,8 @@ public:
     }
 };
 
-class Paper {
+class Paper
+{
 public:
     string paperName;
     vector<classInfo> lectures;
@@ -49,52 +54,60 @@ public:
     vector<classInfo> labs;
     vector<assignment> assignments;
 
-    void addLecture(const classInfo& lecture){
+    void addLecture(const classInfo &lecture)
+    {
         lectures.push_back(lecture);
     }
-    void addTutorial(const classInfo& tutorial){
+    void addTutorial(const classInfo &tutorial)
+    {
         tutorials.push_back(tutorial);
     }
-    void addLab(const classInfo& lab){
+    void addLab(const classInfo &lab)
+    {
         labs.push_back(lab);
     }
-    void addAssignment(const assignment& assignment){
+    void addAssignment(const assignment &assignment)
+    {
         assignments.push_back(assignment);
     }
 
-    void displayPaperInfo(){
+    void displayPaperInfo()
+    {
         std::cout << "Paper: " << paperName << std::endl;
         std::cout << "Lectures: " << std::endl;
-        for (int i = 0; i < lectures.size(); i++){
+        for (int i = 0; i < lectures.size(); i++)
+        {
             lectures[i].displayInfo();
         }
         std::cout << "Tutorials: " << std::endl;
-        for (int i = 0; i < tutorials.size(); i++){
+        for (int i = 0; i < tutorials.size(); i++)
+        {
             tutorials[i].displayInfo();
         }
         std::cout << "Labs: " << std::endl;
-        for (int i = 0; i < labs.size(); i++){
+        for (int i = 0; i < labs.size(); i++)
+        {
             labs[i].displayInfo();
         }
         std::cout << "Assignments: " << std::endl;
-        for (int i = 0; i < assignments.size(); i++){
+        for (int i = 0; i < assignments.size(); i++)
+        {
             std::cout << "Assignment: " << assignments[i].assignmentName << " for " << assignments[i].paperName << " due on day " << assignments[i].dueDate << " with grade " << assignments[i].grade << std::endl;
         }
     }
-
-
 };
 
-void displayMenu(){
+void displayMenu()
+{
     cout << "\n===== Paper Management System =====\n";
     cout << "test\n";
 }
-    
 
-int main() {
+int main()
+{
     displayMenu();
-    //test data
-    // Create a new paper
+    // test data
+    //  Create a new paper
     Paper compSci;
     compSci.paperName = "CS101: Introduction to Computer Science";
 
@@ -115,8 +128,7 @@ int main() {
     compSci.addAssignment(assignment("Data Structures", "Implement a linked list", "Intro to CS", 28, 0));
 
     // Display all information about the paper
-    //compSci.displayPaperInfo();
+    // compSci.displayPaperInfo();
 
     return 0;
 }
-
