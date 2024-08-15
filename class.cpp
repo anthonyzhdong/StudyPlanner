fea863213d20dde2079c7956be512e73f1e1e377
donan928 <donan928@student.otago.ac.nz>
2024-08-15 16:25:16 +1200

    created event class

--- class.cpp
+++ class.cpp
@@ -3,14 +3,53 @@
 #include <string>
 using namespace std;
 
-class paper{
+class classInfo
+{
+public:
+    string paperName;
+    string type;
+    string location;
+    int day;
+    int time;
+    classInfo(string paperName, string type, string location, int day, int time)
+    {
+        this->paperName = paperName;
+        this->type = type;
+        this->location = location;
+        this->day = day;
+        this->time = time;
+    }
+    void displayInfo()
+    {
+        std::cout << type << " for " << paperName << " at " << location << " on day " << day << " at " << time << std::endl;
+    }
+};
 
+class Paper
+{
+public:
+    string paperName;
+    vector<classInfo> lectures;
+    vector<classInfo> tutorials;
+    vector<classInfo> labs;
+    //assignment & exam
 
+    void addLecture(const classInfo &lecture)
+    {
+        lectures.push_back(lecture);
+    }
+    void addTutorial(const classInfo &tutorial)
+    {
+        tutorials.push_back(tutorial);
+    }
+    void addLab(const classInfo &lab)
+    {
+        labs.push_back(lab);
+    }
 
+    void setName(const string &name)
+    {
+        paperName = name;
+    }
 
-
-
-
-
-    
-}
+};
