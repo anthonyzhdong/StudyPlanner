d0dd154d9fbe8314eab2547f436bb4a1a4f454b2
Aidan Cushman <cusai543@student.otago.ac.nz>
2024-08-16 14:46:15 +1200

    Fixed event header file

--- Event.h
+++ Event.h
@@ -1,3 +1,27 @@
-class Event {
-    
+#ifndef EVENT_H
+#define EVENT_H
+
+#include <string>
+
+class Event
+{
+public:
+    // Constructor
+    Event(std::string eventName, int startTime, int endTime, int day, int week);
+
+    // Getters
+    std::string getEventName() const;
+    int getStartTime() const;
+    int getEndTime() const;
+    int getDay() const;
+    int getWeek() const;
+
+private:
+    std::string eventName;
+    int startTime;
+    int endTime;
+    int day;
+    int week;
 };
+
+#endif // EVENT_H
