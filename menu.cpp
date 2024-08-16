bc183354df6ca95f01ad8b8f8540b0748abc4dab
Luke <hoplu238@student.otago.ac.nz>
2024-08-16 16:09:39 +1200

    Added the event calling to the menu to add an event and then that gets added to the calendar

--- menu.cpp
+++ menu.cpp
@@ -3,22 +3,22 @@
 #include <string>
 #include "Calendar.h"
 #include "Event.h"
+#include "AddEvent.cpp"
 using namespace std;
 // g++ -o menu menu.cpp
 // ./menu
-
 
 Calendar calendar = Calendar();
 
 void displayMenu()
 {
     cout << "\n"
-" ____  _             _         ____  _                             \n"
-"/ ___|| |_ _   _  __| |_   _  |  _ \\| | __ _ _ __  _ __   ___ _ __ \n"
-"\\___ \\| __| | | |/ _` | | | | | |_) | |/ _` | '_ \\| '_ \\ / _ \\ '__|\n"
-" ___) | |_| |_| | (_| | |_| | |  __/| | (_| | | | | | | |  __/ |   \n"
-"|____/ \\__|\\__,_|\\__,_|\\__, | |_|   |_|\\__,_|_| |_|_| |_|\\___|_|   \n"
-"                       |___/                                        \n";
+            " ____  _             _         ____  _                             \n"
+            "/ ___|| |_ _   _  __| |_   _  |  _ \\| | __ _ _ __  _ __   ___ _ __ \n"
+            "\\___ \\| __| | | |/ _` | | | | | |_) | |/ _` | '_ \\| '_ \\ / _ \\ '__|\n"
+            " ___) | |_| |_| | (_| | |_| | |  __/| | (_| | | | | | | |  __/ |   \n"
+            "|____/ \\__|\\__,_|\\__,_|\\__, | |_|   |_|\\__,_|_| |_|_| |_|\\___|_|   \n"
+            "                       |___/                                        \n";
     // cout << "Type a command and press enter.\n";
     // cout << "Display item(s): display <papers|lectures|labs|tutorials|assignments>\n";
     // cout << "Add an item: add <paper|lecture|lab|tutorial|assignment>\n"; // After typing this, prompt the user to add details.
@@ -30,19 +30,21 @@ void displayMenu()
     cout << "5. Exit\n";
     int number;
     // have to figure out how to switch between states
-    while(true){
-        if(cin >> number){
-            if(number == 1){
-                //display papers
+    while (true)
+    {
+        if (cin >> number)
+        {
+            if (number == 1)
+            {
+                // display papers
                 cout << "Adding event" << endl;
                 Event e = Event("test", 1, 2, 3, 4);
                 calendar.addEvent(e);
                 break;
-            }else if(number ==2){
-                //display calendar
-                break;
-            }else if(number ==3){
-                //add event
+            }
+            else if (number == 2)
+            {
+                // display calendar
                 break;
             }else if (number == 4){
                 //start study session
@@ -51,14 +53,23 @@ void displayMenu()
                 //exit
                 break;
             }
-        }else{
+            else if (number == 3)
+            {
+                // add event
+                AddEvent eventHandler; // Create an instance of AddEvent
+                calendar.addEvent(eventHandler.AddEventMenu());
+
+                break;
+            }
+        }
+        else
+        {
             cout << "Invalid input. Please enter an integer." << endl;
-            cin.clear(); // Clear error flags
+            cin.clear();                                                   // Clear error flags
             cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Discard invalid input
         }
     }
     cout << "input: " << number << endl;
-
 }
 
 int main()
