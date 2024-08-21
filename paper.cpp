1d9835f49dcd051154a1434f1f226c586ea65004
Luke <hoplu238@student.otago.ac.nz>
2024-08-22 09:28:12 +1200

    Added assignment and exam classes

--- paper.cpp
+++ paper.cpp
@@ -5,6 +5,8 @@
 #include "lecture.h"
 #include "tutorial.h"
 #include "lab.h"
+#include "exam.h"
+#include "assignment.h"
 
 using namespace std;
 
@@ -32,12 +34,12 @@ int paper::getPaperPoints() const
 }
 
 // setter methods
-void paper::setPaperName(const string& paperName)
+void paper::setPaperName(const string &paperName)
 {
     this->paperName = paperName;
 }
 
-void paper::setPaperCode(const string& paperCode)
+void paper::setPaperCode(const string &paperCode)
 {
     this->paperCode = paperCode;
 }
@@ -48,38 +50,42 @@ void paper::setPaperPoints(int paperPoints)
 }
 
 // add lecture, tutorial, lab methods
-void paper::addLecture(const Lecture& lecture)
+void paper::addLecture(const Lecture &lecture)
 {
     lectures.push_back(lecture);
 }
 
-void paper::addTutorial(const Tutorial& tutorial)
+void paper::addTutorial(const Tutorial &tutorial)
 {
     tutorials.push_back(tutorial);
 }
 
-void paper::addLab(const Lab& lab)
+void paper::addLab(const Lab &lab)
 {
     labs.push_back(lab);
 }
 
 // display paper information
-void paper::displayInfo() const {
+void paper::displayInfo() const
+{
     cout << "Paper: " << paperName << " (" << paperCode << ")" << endl;
     cout << "Points: " << paperPoints << endl;
-    
+
     cout << "Lectures:" << endl;
-    for (const auto& lecture : lectures) {
+    for (const auto &lecture : lectures)
+    {
         lecture.displayInfo();
     }
-    
+
     cout << "Labs:" << endl;
-    for (const auto& lab : labs) {
+    for (const auto &lab : labs)
+    {
         lab.displayInfo();
     }
-    
+
     cout << "Tutorials:" << endl;
-    for (const auto& tutorial : tutorials) {
+    for (const auto &tutorial : tutorials)
+    {
         tutorial.displayInfo();
     }
 }
