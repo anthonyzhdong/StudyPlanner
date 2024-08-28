e86a35aa30d22d5ce450af7726bac09fd4f996fc
donan928 <donan928@student.otago.ac.nz>
2024-08-29 09:27:04 +1200

    validation

---
+++ validation.cpp
@@ -0,0 +1,24 @@
+#include "validation.h"
+#include <string>
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+
+int validation::getValidInteger(){
+    return 0;
+}
+
+string validation::getValidString(const std::string &prompt){
+    return "";
+}
+int validation::timeValidation(const std::string &prompt, int minTime){
+    return 0;
+}
+int validation::dayValidation(){
+    return 0;
+}
+int validation::weekValidation(){
+    return 0;
+}
