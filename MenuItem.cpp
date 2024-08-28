98087bd99c9209f696e193d07bae6fabf71bd095
Sam Merton <mersa636@student.otago.ac.nz>
2024-08-29 09:27:05 +1200

    added test for menuitem

--- MenuItem.cpp
+++ MenuItem.cpp
@@ -1,4 +1,5 @@
 #include "MenuItem.h"
+#include <string>
 
 MenuItem::MenuItem(const std::string &name, std::function<void()> action) : name(name), action(action) {}
 
@@ -11,3 +12,23 @@ void MenuItem::execute() const
 {
     action();
 }
+bool testVar = true;
+void testAction() {
+    testVar = false;
+}
+
+bool test() {
+    bool passed = true;
+    MenuItem testItem("Test item", testAction);
+    // Check the name
+    if (testItem.getName() != "Test item")
+        passed = false;
+    // Check if test executes
+    testItem.execute();
+    if (testVar)
+        passed = false;
+
+    return passed;
+
+}
+
