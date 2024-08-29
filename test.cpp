7b5b1e6a17c4fc626609a0a62e0da2a0c2ba10cb
donan928 <donan928@student.otago.ac.nz>
2024-08-29 18:51:02 +1200

    revert test

--- test.cpp
+++ test.cpp
@@ -15,7 +15,7 @@ int main(){
     eventSkeleton testEvent = eventSkeleton(EventType::LECTURE, "COSC345", 1, 1, 930, 1000, "Mellor 3.1");
     validation v = validation();
     //Because it returns either we create a bool called passed then return based on it at end  or we can just do it like this
-    if(calendar.test()&&testEvent.test()&&v.test()){
+    if(calendar.test()&&testEvent.test()){
         std::cout << "Testing Passed" << std::endl;
     }else{
         std::cout << "Testing Failed" << std::endl;
